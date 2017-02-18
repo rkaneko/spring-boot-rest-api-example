defd9d165e8d2c2cafc1c93c9b4b307e9d59cb7c
rkaneko <kanek.o.dev@gmail.com>
2017-02-19 06:38:32 +0900

    `update` database config

--- src/main/java/com/rkaneko/rest/DatabaseConfig.java
+++ src/main/java/com/rkaneko/rest/DatabaseConfig.java
@@ -1,11 +1,26 @@
 package com.rkaneko.rest;
 
 import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 public class DatabaseConfig {
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.datasource.driverClassName}")
+    private String driverClassName;
+    @Value("${spring.database.url}")
+    private String url;
+
     @Bean
     public DataSource dataSource() {
-        return new DataSource();
+        DataSource dataSource = new DataSource();
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        return dataSource;
     }
 }
